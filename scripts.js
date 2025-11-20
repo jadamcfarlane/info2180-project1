011fa026ded1e36ca39d664411c81684bd84a5d9
Jada McFarlane <jada.mcfarlane@mymona.uwi.edu>
2025-11-19 22:56:57 -0500

    Added edited javascript file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const form = document.querySelector(".newsletter form");
+    const emailInput = document.querySelector("#email");
+    const messageDiv = document.querySelector(".message");
+
+    form.addEventListener("submit", function(event){
+        const email = emailInput.ariaValueMax.trim();
+
+        if (email === "") {
+            messageDiv.textContent = "Please enter a valid email address.";
+            messageDiv.style.color = "red";
+        } else {
+            messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+            messageDiv.style.color = "white";
+            form.reset();
+        }
+    })
+})
